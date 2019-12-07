941525ae2094981502a16d35ae9607051d66f96a
Samantha Muellner <shm53@nau.edu>
2019-12-07 09:34:53 -0700

    Moved around some files
    I moved some files from an inner folder to the main directory and added a 'Notes_on_testing.py' that allows us ot see the original code we are basing ours off of

--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
+
 # # Importing the Keras Libraries and Packages
 
 # from keras.models import Sequential # initialize neural network model as a sequential network
@@ -177,17 +178,28 @@ classifier.add(MaxPooling2D(pool_size = (2, 2)))
 classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+classifier.add(Conv2D(32, (3, 3), activation = 'relu'))
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+
+
+
 # Step 3 - Flattening
 classifier.add(Flatten())
 
 # Step 4 - Full connection
 classifier.add(Dense(units = 128, activation = 'relu'))
-classifier.add(Dense(units = 1, activation = 'sigmoid'))
+classifier.add(Dense(4, activation = 'softmax'))
 
 # Compiling the CNN
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+classifier.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
 # Part 2 - Fitting the CNN to the images
 from keras.preprocessing.image import ImageDataGenerator
+
+TRAIN_BATCH_SIZE = 100
+TEST_BATCH_SIZE = 60
 
 train_datagen = ImageDataGenerator(rescale = 1./255,
                                     shear_range = 0.2,
@@ -196,36 +208,44 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
                                     
 test_datagen = ImageDataGenerator(rescale = 1./255)
 
+#training_set = train_datagen.flow_from_directory('dataset/test2/training_set',
+#                                                    target_size = (64, 64),
+#                                                    batch_size = 32,
+#                                                    class_mode = 'categorical')
+
 training_set = train_datagen.flow_from_directory('dataset/training_set',
                                                     target_size = (64, 64),
-                                                    batch_size = 32,
-                                                    class_mode = 'binary')
+                                                    batch_size = TRAIN_BATCH_SIZE,
+                                                    class_mode = 'categorical')
+
+#test_set = test_datagen.flow_from_directory('dataset/test2/testing_set',
+#                                            target_size = (64, 64),
+#                                            batch_size = 32,
+#                                            class_mode = 'categorical')
 
 test_set = test_datagen.flow_from_directory('dataset/test_set',
                                             target_size = (64, 64),
-                                            batch_size = 32,
-                                            class_mode = 'binary')
+                                            batch_size = TEST_BATCH_SIZE,
+                                            class_mode = 'categorical')
+#amount of files in training set
+train_len = 294
 
+#amount of files in test set
+test_len = 94
+
+
+#we want to avoid over fitting because that gives false accuracy
+#   than normally is shown if accuracy is far off from val_accuracy.
+#   The real accuracy is about when the accuracy and val accuracy converge.
 classifier.fit_generator(training_set,
-                            steps_per_epoch = 8000,
-                            epochs = 25,
+                            #NOTE==========Mess with batch sizes to find best accuracy
+                            steps_per_epoch = train_len//TRAIN_BATCH_SIZE,
+                            #mess around with epochs to find a better accuracy
+                            epochs = 50,
                             validation_data = test_set,
-                            validation_steps = 2000)
+                            validation_steps = test_len//TEST_BATCH_SIZE)
 
-# Part 3 - Making new predictions
-import numpy as np
-from keras.preprocessing import image
+target_dir = './models/'
 
-test_image = image.load_img('dataset/single_prediction/cat_or_dog_1.jpg', target_size = (64, 64))
-test_image = image.img_to_array(test_image)
-test_image = np.expand_dims(test_image, axis = 0)
-result = classifier.predict(test_image)
-training_set.class_indices
-
-if result[0][0] == 0:
-    prediction = '1: dog'
-else:
-    prediction = '1: cat'
-
-
-print(prediction)
+classifier.save('./models/model.h5')
+classifier.save_weights('./models/weights.h5')

6ea53512ea3aba236c9ad88121a362ef6d0425e9
Samantha Muellner <shm53@nau.edu>
2020-02-11 09:02:06 -0700

    Working on developing 60-20-20 split
    Working on developing 60-20-20 split for the video data -- will upload the program that succesfully sorts our videos in the next upload

--- testingCNN.py
+++ testingCNN.py
@@ -24,6 +24,7 @@ def predict(file):
     x = load_img(file, target_size=(img_width,img_height))
     x = img_to_array(x)
     x = np.expand_dims(x, axis=0)
+    
     array = model.predict(x)
     result = array[0]
     answer = np.argmax(result)
@@ -31,26 +32,21 @@ def predict(file):
     if answer == 0:
         #print("Predicted Dog Eating")
         print("Predicted Cat")
+
     elif answer == 1:
         #print("Predicted dog running")
         print("Predicted Dog")
+
     elif answer == 2:
         #print("Predicted dog sitting")
-<<<<<<< Updated upstream:executeTest.py
-        print("Predicted Duck")
-    elif answer == 3:
-        #print("Predicted dog sleeping")
-        print("Predicted Koala")
-    else:
-        print("Unable to predict specimen. Need further training")
-=======
         print("Predicted Ducks")
+
     elif answer == 3:
         #print("Predicted dog sleeping")
         print("Predicted Koala")
->>>>>>> Stashed changes:executeTest.py
         
     return answer
+
 
 for i, ret in enumerate(os.walk(test_path)):
     for i, filename in enumerate(ret[2]):
